b39a9b0be0db4b51b6d34a8b8958c0b612fc5c73
Jan Dockx <jandockx@gmail.com>
2017-09-15 09:48:14 +0200

    make select return an instance

--- test/lib/selectTestInstance.js
+++ test/lib/selectTestInstance.js
@@ -9,12 +9,8 @@ describe('selectInstance', function () {
   it('selects according to weight', function () {
     // noinspection JSUnresolvedVariable
     return selectInstance(serviceType, ['test_instance_unit_test0._tcp.dns-sd-lookup.toryt.org']).must.fulfill(details => {
-      details.must.be.an.array()
-      details[0].forEach(d => { d.selected = 0 })
-      for (let i = 0; i < 10000000; i++) {
-        details[0].select().selected++
-      }
-      console.log(details[0].map(d => ({weight: d.weight, selected: d.selected})))
+      details.must.be.an.object()
+      console.log(details)
     })
   })
 })
