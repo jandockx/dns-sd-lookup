f6d60d3d17d690d5d96a1142442d2573210ed4da
Jan Dockx <jandockx@gmail.com>
2017-09-16 15:46:02 +0200

    add extract.protocol

--- test/lib/extractTest.js
+++ test/lib/extractTest.js
@@ -3,29 +3,47 @@
 const extract = require('../../lib/extract')
 const x = require('cartesian')
 
+const domain = 'dns-sd-lookup.toryt.org'
+const protocols = ['tcp', 'udp']
+const serviceType = '_a-service-type'
+// noinspection SpellCheckingInspection
+const prefix = [
+  '',
+  '_a-sub-service._sub',
+  '_a.complex.sub.service._sub',
+  'a-service-instance',
+  'A Human Readable Sérvice Type'
+]
+const services = prefix.map(p => p ? p + '.' + serviceType : serviceType)
+
 describe('extract', function () {
   describe('#domain', function () {
-    const domain = 'dns-sd-lookup.toryt.org'
-    const protocols = ['_tcp', '_udp']
-    const serviceType = '_a-service-type'
-    // noinspection SpellCheckingInspection
-    const prefix = [
-      '',
-      '_a-sub-service._sub',
-      '_a.complex.sub.service._sub',
-      'a-service-instance',
-      'A Human Readable Sérvice Type'
-    ]
     // noinspection JSUnresolvedFunction
     const fqdns = x({
-      service: prefix.map(p => p ? p + '.' + serviceType : serviceType),
+      service: services,
       protocol: protocols
-    }).map(c => `${c.service}.${c.protocol}.${domain}`)
+    }).map(c => `${c.service}._${c.protocol}.${domain}`)
     fqdns.forEach(fqdn => {
       it(`works as expected for ${fqdn}`, function () {
         const result = extract.domain(fqdn)
         console.log('%s --> %s', fqdn, result)
         result.must.equal(domain)
+      })
+    })
+  })
+
+  describe('#protocol', function () {
+    const cases = x({
+      service: services,
+      protocol: protocols
+    })
+    // noinspection JSUnresolvedFunction
+    cases.forEach(c => {
+      const fqdn = `${c.service}._${c.protocol}.${domain}`
+      it(`works as expected for ${fqdn}`, function () {
+        const result = extract.protocol(fqdn)
+        console.log('%s --> %s', fqdn, result)
+        result.must.equal(c.protocol)
       })
     })
   })
