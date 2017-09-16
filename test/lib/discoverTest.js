d2f46ce1d5ff02b00442879ab4cd62576f14b7a1
Jan Dockx <jandockx@gmail.com>
2017-09-17 00:29:46 +0200

    use ServiceInstance in lookup and discover skeletons - doesn’t work because the example is bad

--- test/lib/discoverTest.js
+++ test/lib/discoverTest.js
@@ -1,6 +1,7 @@
 /* eslint-env mocha */
 
 const discover = require('../../lib/discover')
+const ServiceInstance = require('../../lib/ServiceInstance')
 
 // noinspection SpellCheckingInspection
 const serviceType = '_test._sub._unittest._tcp.dns-sd-lookup.toryt.org'
@@ -10,6 +11,7 @@ describe('discover', function () {
     // noinspection JSUnresolvedVariable
     return discover(serviceType).must.fulfill(details => {
       details.must.be.an.array()
+      details.forEach(d => d.must.be.an.instanceof(ServiceInstance))
       console.log(details)
     })
   })
