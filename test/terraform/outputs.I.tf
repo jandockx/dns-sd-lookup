082c68ea1bb4b0811663c347d21ea00287d3916d
Jan Dockx <jandockx@gmail.com>
2017-09-17 23:32:59 +0200

    add output for new cases

--- test/terraform/outputs.I.tf
+++ test/terraform/outputs.I.tf
@@ -13,3 +13,15 @@ output "I-zone_ns" {
 output "I-type_with_1_instance_no_subtype-instance" {
   value = "${module.instance-type_with_1_instance_no_subtype.II-instance}"
 }
+
+output "I-double_txt-instance" {
+  value = "${module.instance-double_txt.II-instance}"
+}
+
+output "I-double_srv-instance" {
+  value = "${module.instance-double_srv.II-instance}"
+}
+
+output "I-double_txt_srv-instance" {
+  value = "${module.instance-double_txt_srv.II-instance}"
+}
