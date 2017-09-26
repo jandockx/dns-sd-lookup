808e3aee007be831bb3ed296d2de0c60ba2f9985
Jan Dockx <jandockx@gmail.com>
2017-09-26 21:04:27 +0200

    add contract for discover and first working test

--- test/lib/discoverTest.js
+++ test/lib/discoverTest.js
@@ -26,24 +26,27 @@
 
 const discover = require('../../lib/discover')
 const ServiceInstance = require('../../lib/ServiceInstance')
+// noinspection JSUnresolvedVariable
+const discoverContract = discover.contract
 
 // noinspection SpellCheckingInspection
-const serviceType = '_type-1-instance-no-subtype._tcp.dns-sd-lookup.toryt.org'
+const serviceType = '_t1i-no-sub._tcp.dns-sd-lookup.toryt.org'
 
 describe('discover', function () {
   it('works in the nominal case', function () {
     // noinspection JSUnresolvedVariable
-    return discover(serviceType).must.fulfill(details => {
+    return discover(serviceType).must.fulfill(discoverContract.resolved.implementation(details => {
       details.must.be.an.array()
       details.forEach(d => d.must.be.an.instanceof(ServiceInstance))
       console.log(details)
-    })
+    }))
   })
   it('works with a death in the nominal case', function () { // MUDO
     // noinspection JSUnresolvedVariable
-    return discover(serviceType, ['test_instance_unit_test1a._tcp.dns-sd-lookup.toryt.org']).must.fulfill(details => {
-      details.must.be.an.array()
-      console.log(details)
-    })
+    return discover(serviceType, ['test_instance_unit_test1a._tcp.dns-sd-lookup.toryt.org'])
+      .must.fulfill(discoverContract.resolved(details => {
+        details.must.be.an.array()
+        console.log(details)
+      }))
   })
 })
