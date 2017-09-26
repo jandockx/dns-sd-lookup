0a00558e5c07cf602248f426cc9aa79b4d24268c
Jan Dockx <jandockx@gmail.com>
2017-09-26 08:22:55 +0200

    fix: don't remember the chars of the service name; specify that dots and backslashes must be escaped

--- test/lib/validateTest.js
+++ test/lib/validateTest.js
@@ -41,8 +41,8 @@ const serviceSubTypes = [
 const instances = [
   'a-service-instance',
   'A Human Readable Sérvice Instance',
-  'instances.with.different.labels',
   'instance.withPartThatIsNotLongerThanIsAcceptableWhichIs63CharactersLabel.lastLabel'
+  'instances\\.with\\.escaped\\\\dots\\\\and\\.slashes',
 ]
 
 function createService (prefix) {
