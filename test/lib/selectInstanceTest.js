5fe63a66096cb988a241115d7006144074e00a97
Jan Dockx <jandockx@gmail.com>
2017-09-30 16:14:50 +0200

    refactor test to reuse distribution test code

--- test/lib/selectInstanceTest.js
+++ test/lib/selectInstanceTest.js
@@ -31,8 +31,83 @@ const notOneOf = selectInstance.notOneOf
 const serviceTypePostfix = '._tcp.dns-sd-lookup.toryt.org'
 const serviceType1InstanceNoSubtype = '_t1i-no-sub' + serviceTypePostfix
 const serviceType1InstanceSubtype = '_subtype._sub._t7i-sub' + serviceTypePostfix
-const serviceType5InstancesWithWeight = '_t8i-5inst' + serviceTypePostfix
+const serviceTypeNInstancesWithWeight = '_t8i-n-inst' + serviceTypePostfix
+const manyInstanceCount = 12
 const must = require('must')
+
+const batch = 16
+
+function testDistribution (timerLabel, deaths, expected) {
+  const expectedPattern = Object.keys(expected).join('|')
+  const matchExpr = new RegExp(`(${expectedPattern})\\.${serviceTypeNInstancesWithWeight.replace(/\./g, '\\.')}`)
+
+  function totalCount (selections) {
+    return Object.keys(selections).reduce(
+      (acc, key) => {
+        acc += selections[key]
+        return acc
+      },
+      0
+    )
+  }
+
+  const expectedStr = Object.keys(expected).map(k => `${k} = ${expected[k] * 100}%`).join(', ')
+
+  function report (selections) {
+    const total = totalCount(selections)
+    console.log(`${total} - expected ${expectedStr}`)
+    Object.keys(selections).forEach(k => {
+      console.log('  %s: %d/%d (%f%%)', k, selections[k], total, Math.round(selections[k] * 1000 / total) / 10)
+    })
+    console.log()
+  }
+
+  function chain (todo, previous) {
+    if (todo <= 0) {
+      return previous
+    }
+    return chain(
+      todo - batch,
+      previous.then(selections => {
+        const next = []
+        for (let i = 0; i < batch; i++) {
+          // noinspection JSUnresolvedFunction
+          next.push(
+            selectInstance(serviceTypeNInstancesWithWeight, notOneOf(deaths))
+              .then(selectionInstanceContract.resolved.implementation(selection => {
+                selection.must.be.an.object()
+                selection.instance.must.match(matchExpr)
+                selections[selection.instance]
+                  ? selections[selection.instance]++
+                  : selections[selection.instance] = 1
+              }))
+          )
+        }
+        return Promise.all(next).then(() => {
+          report(selections)
+          return selections
+        })
+      })
+    )
+  }
+
+  console.time(timerLabel)
+
+  return chain(512, Promise.resolve({}))
+    .then(selections => {
+      console.timeEnd(timerLabel)
+      const total = totalCount(selections)
+      Object.keys(expected).forEach(e => {
+        Math.abs(expected[e] - (selections[e + '.' + serviceTypeNInstancesWithWeight] / total)).must.be.below(0.05)
+        /* NOTE: I would like for the deviation from the expected weight distribution to be less than 2.5%.
+                 When testing with 2 instances, in 1024 tries, if often happens that the deviation that the deviation
+                 is larger. That is surprising. This would mean that a random choice is not good enough.
+                 That would imply that we rather need some sort of memory, which would be bad.
+                 After this observation, the limit was lowered to 5%, and the tries are lowered to 512, for
+                 test speed reasons. */
+      })
+    })
+}
 
 describe('selectInstance', function () {
   it('works in the nominal case with 1 instance, without a subtype', function () {
@@ -53,12 +128,14 @@ describe('selectInstance', function () {
         console.log(selection)
       }))
   })
-  it('works in the nominal case, with 5 instances', function () {
+  it(`works in the nominal case, with ${manyInstanceCount} instances`, function () {
+    this.timeout(10000)
+
     // noinspection JSUnresolvedVariable
-    return selectInstance(serviceType5InstancesWithWeight)
+    return selectInstance(serviceTypeNInstancesWithWeight)
       .must.fulfill(selectionInstanceContract.resolved.implementation(selection => {
         selection.must.be.an.object()
-        selection.instance.must.equal('instance 8a.' + serviceType5InstancesWithWeight)
+        selection.instance.must.equal('instance 8a.' + serviceTypeNInstancesWithWeight)
         console.log(selection)
       }))
   })
@@ -71,113 +148,60 @@ describe('selectInstance', function () {
       }))
   })
   it('selects according to weight with a filter', function () {
+    this.timeout(10000)
+
     const deaths = [
-      `Instance 8a.${serviceType5InstancesWithWeight}`,
-      `Instance 8d.${serviceType5InstancesWithWeight}`
+      `Instance 8a.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8d.${serviceTypeNInstancesWithWeight}`
     ]
     // noinspection JSUnresolvedVariable
-    return selectInstance(serviceType5InstancesWithWeight, notOneOf(deaths))
+    return selectInstance(serviceTypeNInstancesWithWeight, notOneOf(deaths))
       .must.fulfill(selectionInstanceContract.resolved.implementation(selection => {
         selection.must.be.an.object()
-        selection.instance.must.match(`instance 8b.${serviceType5InstancesWithWeight}`)
+        selection.instance.must.match(`instance 8b.${serviceTypeNInstancesWithWeight}`)
         console.log(selection)
       }))
   })
   it('selects according to weight with a filter that excludes every instance', function () {
     const deaths = [
-      `Instance 8a.${serviceType5InstancesWithWeight}`,
-      `Instance 8d.${serviceType5InstancesWithWeight}`,
-      `Instance 8c.${serviceType5InstancesWithWeight}`,
-      `Instance 8b.${serviceType5InstancesWithWeight}`,
-      `Instance 8e.${serviceType5InstancesWithWeight}`
+      `Instance 8c.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8e.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8c.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8a.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8f.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8e.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8j.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8i.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8e.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8g.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8k.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8h.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8d.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8l.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8b.${serviceTypeNInstancesWithWeight}`
     ]
     // noinspection JSUnresolvedVariable
-    return selectInstance(serviceType5InstancesWithWeight, notOneOf(deaths))
+    return selectInstance(serviceTypeNInstancesWithWeight, notOneOf(deaths))
       .must.fulfill(selectionInstanceContract.resolved.implementation(selection => {
         must(selection).be.null()
         console.log(selection)
       }))
   })
-  it('selects according to weight with a filter evenly', function () {
-    const batch = 16
+
+  const labelA = 'selects according to weight with a filter evenly'
+  it(labelA, function () {
     this.timeout(60000)
-    const timerLabel = 'selects according to weight evenly'
 
     const deaths = [
-      `Instance 8a.${serviceType5InstancesWithWeight}`,
-      `Instance 8b.${serviceType5InstancesWithWeight}`
+      `Instance 8a.${serviceTypeNInstancesWithWeight}`,
+      `Instance 8b.${serviceTypeNInstancesWithWeight}`
     ]
 
-    const matchExpr = new RegExp(`(instance 8c|instance 8d)\\.${serviceType5InstancesWithWeight.replace(/\./g, '\\.')}`)
-
     const expected = {}
-    expected[`instance 8c.${serviceType5InstancesWithWeight}`] = 0.3
-    expected[`instance 8d.${serviceType5InstancesWithWeight}`] = 0.7
+    expected[`instance 8c`] = 0.3
+    expected[`instance 8d`] = 0.7
 
-    function totalCount (selections) {
-      return Object.keys(selections).reduce(
-        (acc, key) => {
-          acc += selections[key]
-          return acc
-        },
-        0
-      )
-    }
-
-    function report (selections) {
-      const total = totalCount(selections)
-      console.log(`${total} - expected c = 30%, d = 70%`)
-      Object.keys(selections).forEach(k => {
-        console.log('  %s: %d/%d (%f%%)', k, selections[k], total, Math.round(selections[k] * 1000 / total) / 10)
-      })
-      console.log()
-    }
-    console.time(timerLabel)
-
-    function chain (todo, previous) {
-      if (todo <= 0) {
-        return previous
-      }
-      return chain(
-        todo - batch,
-        previous
-          .then(selections => {
-            const next = []
-            for (let i = 0; i < batch; i++) {
-              // noinspection JSUnresolvedFunction
-              next.push(
-                selectInstance(serviceType5InstancesWithWeight, notOneOf(deaths))
-                  .then(selectionInstanceContract.resolved.implementation(selection => {
-                    selection.must.be.an.object()
-                    selection.instance.must.match(matchExpr)
-                    selections[selection.instance]
-                      ? selections[selection.instance]++
-                      : selections[selection.instance] = 1
-                  }))
-              )
-            }
-            return Promise.all(next).then(() => {
-              report(selections)
-              return selections
-            })
-          })
-      )
-    }
-
-    return chain(512, Promise.resolve({}))
-      .then(selections => {
-        console.timeEnd(timerLabel)
-        const total = totalCount(selections)
-        Object.keys(expected).forEach(e => {
-          Math.abs(expected[e] - (selections[e] / total)).must.be.below(0.05)
-          /* NOTE: I would like for the deviation from the expected weight distribution to be less than 2.5%.
-                   When testing with 2 instances, in 1024 tries, if often happens that the deviation that the deviation
-                   is larger. That is surprising. This would mean that a random choice is not good enough.
-                   That would imply that we rather need some sort of memory, which would be bad.
-                   After this observation, the limit was lowered to 5%, and the tries are lowered to 512, for
-                   test speed reasons. */
-        })
-      })
+    return testDistribution(labelA, deaths, expected)
   })
 
   let failures = [
