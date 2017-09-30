b1310391525e8449023d015a1398418dde23ac20
Jan Dockx <jandockx@gmail.com>
2017-09-30 14:30:31 +0200

    add more nominal tests

--- test/lib/selectInstanceTest.js
+++ test/lib/selectInstanceTest.js
@@ -28,50 +28,76 @@ const selectInstance = require('../../lib/selectInstance')
 const selectionInstanceContract = selectInstance.contract
 const notOneOf = selectInstance.notOneOf
 
-// noinspection SpellCheckingInspection
 const serviceTypePostfix = '._tcp.dns-sd-lookup.toryt.org'
-// noinspection SpellCheckingInspection
-const serviceType1Instance = '_t1i-no-sub' + serviceTypePostfix
-// noinspection SpellCheckingInspection
-const serviceTypeWithWeight = '_t8i-5inst' + serviceTypePostfix
+const serviceType1InstanceNoSubtype = '_t1i-no-sub' + serviceTypePostfix
+const serviceType1InstanceSubtype = '_subtype._sub._t7i-sub' + serviceTypePostfix
+const serviceType5InstancesWithWeight = '_t8i-5inst' + serviceTypePostfix
+const must = require('must')
 
 describe('selectInstance', function () {
-  it('works in the nominal case, without a subtype', function () {
+  it('works in the nominal case with 1 instance, without a subtype', function () {
     // noinspection JSUnresolvedVariable
-    return selectInstance(serviceType1Instance)
+    return selectInstance(serviceType1InstanceNoSubtype)
+      .must.fulfill(selectionInstanceContract.resolved.implementation(selection => {
+        selection.must.be.an.object()
+        selection.instance.must.equal('instance 1.' + serviceType1InstanceNoSubtype)
+        console.log(selection)
+      }))
+  })
+  it('works in the nominal case with 1 instance, with a subtype', function () {
+    // noinspection JSUnresolvedVariable
+    return selectInstance(serviceType1InstanceSubtype)
+      .must.fulfill(selectionInstanceContract.resolved.implementation(selection => {
+        selection.must.be.an.object()
+        selection.instance.must.equal('instance 7._t7i-sub' + serviceTypePostfix)
+        console.log(selection)
+      }))
+  })
+  it('works in the nominal case, with 5 instances', function () {
+    // noinspection JSUnresolvedVariable
+    return selectInstance(serviceType5InstancesWithWeight)
+      .must.fulfill(selectionInstanceContract.resolved.implementation(selection => {
+        selection.must.be.an.object()
+        selection.instance.must.equal('instance 8a.' + serviceType5InstancesWithWeight)
+        console.log(selection)
+      }))
+  })
+  it('resolves to null with a non-existent service type', function () {
+    // noinspection JSUnresolvedVariable
+    return selectInstance('_not-exist' + serviceTypePostfix)
+      .must.fulfill(selectionInstanceContract.resolved.implementation(selection => {
+        must(selection).be.null()
+        console.log(selection)
+      }))
+  })
+  it('selects according to weight with a filter', function () {
+    const deaths = [
+      `Instance 8a.${serviceType5InstancesWithWeight}`,
+      `Instance 8d.${serviceType5InstancesWithWeight}`
+    ]
+    // noinspection JSUnresolvedVariable
+    return selectInstance(serviceType5InstancesWithWeight, notOneOf(deaths))
       .must.fulfill(selectionInstanceContract.resolved.implementation(instance => {
         instance.must.be.an.object()
+        instance.instance.must.match(`instance 8b.${serviceType5InstancesWithWeight}`)
         console.log(instance)
       }))
   })
-  it('selects according to weight', function () {
-    const deaths = [
-      `Instance 8a.${serviceTypeWithWeight}`,
-      `Instance 8d.${serviceTypeWithWeight}`
-    ]
-    // noinspection JSUnresolvedVariable
-    return selectInstance(serviceTypeWithWeight, notOneOf(deaths))
-      .must.fulfill(selectionInstanceContract.resolved.implementation(details => {
-        details.must.be.an.object()
-        details.instance.must.match(`instance 8b.${serviceTypeWithWeight}`)
-        console.log(details)
-      }))
-  })
-  it('selects according to weight evenly', function () {
+  it('selects according to weight with a filter evenly', function () {
     const batch = 16
     this.timeout(60000)
     const timerLabel = 'selects according to weight evenly'
 
     const deaths = [
-      `Instance 8a.${serviceTypeWithWeight}`,
-      `Instance 8b.${serviceTypeWithWeight}`
+      `Instance 8a.${serviceType5InstancesWithWeight}`,
+      `Instance 8b.${serviceType5InstancesWithWeight}`
     ]
 
-    const matchExpr = new RegExp(`(instance 8c|instance 8d)\\.${serviceTypeWithWeight.replace(/\./g, '\\.')}`)
+    const matchExpr = new RegExp(`(instance 8c|instance 8d)\\.${serviceType5InstancesWithWeight.replace(/\./g, '\\.')}`)
 
     const expected = {}
-    expected[`instance 8c.${serviceTypeWithWeight}`] = 0.3
-    expected[`instance 8d.${serviceTypeWithWeight}`] = 0.7
+    expected[`instance 8c.${serviceType5InstancesWithWeight}`] = 0.3
+    expected[`instance 8d.${serviceType5InstancesWithWeight}`] = 0.7
 
     function totalCount (selections) {
       return Object.keys(selections).reduce(
@@ -105,7 +131,7 @@ describe('selectInstance', function () {
             for (let i = 0; i < batch; i++) {
               // noinspection JSUnresolvedFunction
               next.push(
-                selectInstance(serviceTypeWithWeight, notOneOf(deaths))
+                selectInstance(serviceType5InstancesWithWeight, notOneOf(deaths))
                   .then(selectionInstanceContract.resolved.implementation(selection => {
                     selection.must.be.an.object()
                     selection.instance.must.match(matchExpr)
